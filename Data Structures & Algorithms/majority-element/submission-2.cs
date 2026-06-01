1dd6fc2487a5653bb8f8dfb9974e0e4226764f40
Giriprasad S Patil <65657199+Giriprasad013-git@users.noreply.github.com>
2026-06-01 15:30:32 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        Dictionary<int,int> freq = new Dictionary<int,int>();
+
+        foreach(int n in nums){
+            if(freq.ContainsKey(n)){
+                freq[n]++;
+            }else{
+                freq.Add(n,1);
+            }
+
+            if(freq[n]>nums.Length/2){
+                return n;
+            }
+        }
+        return 0;
+    }
+}
