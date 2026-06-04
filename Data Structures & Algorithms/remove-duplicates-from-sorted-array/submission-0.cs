af20075f0fa9732577c384429c68476e71c051fe
Giriprasad S Patil <65657199+Giriprasad013-git@users.noreply.github.com>
2026-06-04 12:46:18 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int slow = 1;
+        for(int fast = 1;fast<nums.Length;fast++){
+            if(nums[fast]!=nums[fast-1]){
+                nums[slow++] = nums[fast];
+            }
+        }
+        return slow;
+    }
+}
